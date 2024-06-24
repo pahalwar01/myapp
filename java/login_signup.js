7c63865c9a1a73d4698af472d5435b52b113905e
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-06-24 23:35:30 +0530

    Update login_signup.js

--- java/login_signup.js
+++ java/login_signup.js
@@ -1,2 +1,18 @@
 var signup_frm = document.getElementById("signup_frm");
 
+signup_frm.onsubmit = function()
+{
+    var user = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var pass = document.getElementById("password").value;
+
+    var user_object_data = {username:user,email:email,phone:phone,password:pass};
+    var user_text_data = JSON.stringify(user_object_data);
+
+    if (user != "" && email != "" && phone != "" && pass !="")
+        {
+            localStorage.setItem(email,user_text_data);
+        }
+
+}
