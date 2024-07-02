435724c284831f160adf42103defd80634b20ffa
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-07-02 12:34:02 +0530

    Update works.js

--- users/user_1/apps/works/js/works.js
+++ users/user_1/apps/works/js/works.js
@@ -13,4 +13,10 @@ add_item.onclick = function()
 {
     var bg = document.getElementById("jobs_bg");
     bg.style.display = "block";
+    var user_email = sessionStorage.getItem("user");
+    var json_data = localStorage.getItem(user_email);
+    var obj_data = JSON.parse(json_data);
+    var rider = document.getElementById("r_name");
+    rider.value = atob(obj_data.username);
+    
 }
