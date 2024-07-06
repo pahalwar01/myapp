b664f110d3ad2bd96ee927f7486e78dcad77443c
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-07-06 15:17:45 +0530

    Create login.js

---
+++ java/login.js
@@ -0,0 +1,88 @@
+// email validation coding start
+
+var email_input = document.getElementById("email");
+email_input.onchange = function()
+{
+    var email = btoa(document.getElementById("email").value);
+    var warning = document.getElementById("email_notice");
+    var signup_btn = document.getElementById("signup_btn");
+ 
+    if (localStorage.getItem(email) != null)
+        {
+            warning.style.display = "block";
+            email_input.style.borderBottomColor = "red";
+            signup_btn.disabled = true;
+            signup_btn.style.background = "#ccc";
+
+            email_input.onclick = function()
+            {
+                email_input.value = "";
+                email_input.style.borderBottomColor = "#ccc";
+                warning.style.display = "none";
+                signup_btn.disabled = false;
+                signup_btn.style.background = "linear-gradient(to right, #E100FF, #7F00FF)";
+
+            }
+
+        }
+
+}
+
+// email validation coding end
+
+// login coding start
+
+var login_frm = document.getElementById("login_frm");
+
+
+login_frm.onsubmit = function()
+{
+    var email =document.getElementById("login_uname");
+    var password = document.getElementById("login_pwd");
+    var email_war = document.getElementById("email_warning");
+    var pass_war = document.getElementById("password_warning");
+
+    if(localStorage.getItem(btoa(email.value)) == null)
+        {
+            email_war.style.display = "block";
+            email.style.borderBottomColor = "red";
+
+            email.onclick = function()
+            {
+                email.value = "";
+                email_war.style.display = "none";
+                email.style.borderBottomColor = "#ccc";
+            }
+        }
+        else
+        {
+            var text_data = localStorage.getItem(btoa(email.value));
+            var object_data = JSON.parse(text_data);
+            var correct_email = object_data.email;
+            var correct_password = object_data.password;
+
+            if(btoa(email.value) == correct_email)
+                {
+                    if(btoa(password.value) == correct_password)
+                        {
+                            sessionStorage.setItem("user",btoa(email.value));
+                            location.replace("users/user_1/user_1.html");
+                        }
+                        else
+                        {
+                            pass_war.style.display = "block";
+                            password.style.borderBottomColor = "red";
+
+                            password.onclick = function()
+                            {
+                                password.value = "";
+                                pass_war.style.display = "none";
+                                password.style.borderBottomColor = "#ccc";
+                            }
+                        }
+                }
+        }
+        return false;
+}
+
+// login coding end
