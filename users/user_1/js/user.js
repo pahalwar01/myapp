eccc63dca1dc8ee3884b11d549191a992895ce1a
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-07-06 17:32:53 +0530

    Create user.js

---
+++ users/user_1/js/user.js
@@ -0,0 +1,31 @@
+window.onload = function(){
+    if(sessionStorage.getItem("user") == null)
+        {
+            window.location.replace("../../index.html");
+        }
+        else
+        {
+            var user_email = sessionStorage.getItem("user");
+            var json_data = localStorage.getItem(user_email);
+            var obj_data = JSON.parse(json_data);
+            var profile_name = document.getElementById("profile_name");
+            var rider_details = document.getElementById("rider_details");
+            
+            rider_details.innerHTML = atob(obj_data.username) + "'s Total Conveyance Details"
+            profile_name.innerHTML = "Welcome " + atob(obj_data.username);
+        }
+}
+
+// Logout coding start
+
+var logout = document.getElementById("logout");
+
+logout.onclick = function()
+{  
+    sessionStorage.clear();
+    var logout_text = document.getElementById("logout_text");
+    logout_text.innerHTML = "Please wait...";
+    setTimeout(function(){window.location = "../../index.html"},2000);
+}
+
+// Logout coding end
