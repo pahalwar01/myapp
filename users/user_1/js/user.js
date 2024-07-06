a805b69a57f12351b9fb520268945488291b1fcc
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-07-06 19:27:29 +0530

    Update user.js

--- users/user_1/js/user.js
+++ users/user_1/js/user.js
@@ -1,19 +1,87 @@
-window.onload = function()
-        {
-            if(sessionStorage.getItem("user") == null)
-            {
-                var admin = user =="admin";
-                var first = user=="Dharampal";
-                var second = user=="Lalit";
-                var third = user=="Karan";
-                var user = admin/first/second/third;
-                var profile_name = document.getElementById("profile_name");
-                var rider_details = document.getElementById("rider_details");
-                
-                rider_details.innerHTML = user + "'s Total Conveyance Details"
-                profile_name.innerHTML = "Welcome " + user;
-            }
+var signup_frm = document.getElementById("signup_frm");
+
+signup_frm.onsubmit = function()
+{
+    var user = btoa(document.getElementById("username").value);
+    var email = btoa(document.getElementById("email").value);
+    var phone = btoa(document.getElementById("phone").value);
+    var pass = btoa(document.getElementById("password").value);
+
+    var user_object_data = {username:user,email:email,phone:phone,password:pass};
+    var user_text_data = JSON.stringify(user_object_data);
+
+    if (user != "" && email != "" && phone != "" && pass !="")
+        {   
+            localStorage.setItem(email,user_text_data);
+            var signup_btn = document.getElementById("signup_btn");
+            signup_btn.style.background = "#14b129";
+            signup_btn.innerHTML = "<i class='fa fa-check' aria-hidden='true'></i> Sign up Successful !😊";
+            setTimeout(function() {
+                signup_btn.style.background = "linear-gradient(to right, #E100FF, #7F00FF)";
+                signup_btn.innerHTML = "Sign up";
+                signup_frm.reset();
+            }, 3000);
+            return false
         }
+
+}
+
+
+
+
+// email validation coding start
+
+var email_input = document.getElementById("email");
+email_input.onchange = function()
+{
+    var email = btoa(document.getElementById("email").value);
+    var warning = document.getElementById("email_notice");
+    var signup_btn = document.getElementById("signup_btn");
+ 
+    if (localStorage.getItem(email) != null)
+        {
+            warning.style.display = "block";
+            email_input.style.borderBottomColor = "red";
+            signup_btn.disabled = true;
+            signup_btn.style.background = "#ccc";
+
+            email_input.onclick = function()
+            {
+                email_input.value = "";
+                email_input.style.borderBottomColor = "#ccc";
+                warning.style.display = "none";
+                signup_btn.disabled = false;
+                signup_btn.style.background = "linear-gradient(to right, #E100FF, #7F00FF)";
+
+            }
+
+        }
+
+}
+
+// email validation coding end
+
+
+window.onload = function(){
+    if(sessionStorage.getItem("user") == null)
+        {
+            window.location.replace("../../index.html");
+        }
+        else
+        {
+            var user_email = sessionStorage.getItem("user");
+            var json_data = localStorage.getItem(user_email);
+            var obj_data = JSON.parse(json_data);
+            var profile_name = document.getElementById("profile_name");
+            var rider_details = document.getElementById("rider_details");
+            
+            rider_details.innerHTML = atob(obj_data.username) + "'s Total Conveyance Details"
+            profile_name.innerHTML = "Welcome " + atob(obj_data.username);
+        }
+}
+
+
+
 
 // Logout coding start
 
