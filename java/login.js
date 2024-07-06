831cf3d04156f7ce268e40a6b9afffb90f3a1e00
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-07-06 15:34:31 +0530

    Update login.js

--- java/login.js
+++ java/login.js
@@ -30,10 +30,11 @@ email_input.onchange = function()
 
 // email validation coding end
 
+
+/*
 // login coding start
 
 var login_frm = document.getElementById("login_frm");
-
 
 login_frm.onsubmit = function()
 {
@@ -86,3 +87,39 @@ login_frm.onsubmit = function()
 }
 
 // login coding end
+*/
+
+var login_frm = document.getElementById("login_frm");
+login_frm.onsubmit = function()
+{
+    function login()
+{
+    var user = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+    var admin = user =="admin" && pass =="admin@123"? "login sucess":"Failed";
+    var first = user=="dharampal" && pass=="dharampal@123"? "login sucess": "Failed";
+    var second = user=="lalit" && pass=="lalit@123"? "login sucess": "Failed";
+    var third = user=="karan" && pass=="karan@123"? "login sucess": "Failed";
+    if (admin=="login sucess") {
+        window.location.href='adminmain.html';
+    } 
+    if (first=="login sucess") {
+        window.location.href='dharampalmain.html';
+    } 
+    if (second=="login sucess") {
+        window.location.href='lalitmain.html';
+    } 
+    if (third=="login sucess") {
+        window.location.href='karanmain.html';
+    }
+    return false;
+}
+
+//Logout Code
+var logout = document.getElementById("logout");
+logout.onclick = function()
+{
+    sessionStorage.clear();
+    
+}
+}
