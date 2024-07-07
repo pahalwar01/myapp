95cde444f05f898aa87a909f9420f0ca66fc8bf4
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-07-07 17:19:34 +0530

    Update java.js

--- java/java.js
+++ java/java.js
@@ -1,15 +1,15 @@
-var login_btn = document.getElementById("login_link");
-var signup_btn = document.getElementById("signup_link");
+var login_link = document.getElementById("login_link");
+var signup_link = document.getElementById("signup_link");
 var signup_box = document.getElementById("signup");
 var login_box = document.getElementById("login");
     
-login_btn.onclick = function()
+login_link.onclick = function()
 {
     signup_box.style.display = "none";
     login_box.style.display = "block";
 }
 
-signup_btn.onclick = function()
+signup_link.onclick = function()
 {
     login_box.style.display = "none";
     signup_box.style.display = "block";
