534afe5701561e2397a8302db5980bf4f7d4871e
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-07-13 18:09:46 +0530

    Update googlesignin.js

--- java/googlesignin.js
+++ java/googlesignin.js
@@ -1,8 +1,9 @@
-import { getAuth, signInWithRedirect, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
-
-const firebaseConfig = {
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+ import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+ import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+ 
+ const firebaseConfig = {
     apiKey: "AIzaSyDXIbUIbKYmKVPUtfrzCPAxLb2t4r_Jlww",
     authDomain: "account-f83b4.firebaseapp.com",
     databaseURL: "https://account-f83b4-default-rtdb.firebaseio.com",
@@ -11,55 +12,82 @@ const firebaseConfig = {
     messagingSenderId: "374695878902",
     appId: "1:374695878902:web:72ea054fbd780f98650cd7",
     measurementId: "G-D0XY2VX61J"
-  };
+ };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-auth.languageCode = 'en';
-const provider = new GoogleAuthProvider();
-const googlelogin = document.getElementById("google_signin");
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
 
-googlelogin.addEventListener("click", function() {
-    signInWithRedirect (auth, provider)
-    .then((result) => {
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const user = result.user;
-        console.log(user);
-        window.location.href="users/user_1/user_1.html";
-         
+ function showMessage(message, divId){
+    var messageDiv=document.getElementById(divId);
+    messageDiv.style.display="block";
+    messageDiv.innerHTML=message;
+    messageDiv.style.opacity=1;
+    setTimeout(function(){
+        messageDiv.style.opacity=0;
+    },5000);
+ }
+ const signUp=document.getElementById('signup_btn');
+ signUp.addEventListener('click', (event)=>{
+    event.preventDefault();
+    const email=document.getElementById('email').value;
+    const password=document.getElementById('password').value;
+    const firstName=document.getElementById('fName').value;
+    const lastName=document.getElementById('lName').value;
 
+    const auth=getAuth();
+    const db=getFirestore();
 
-    }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        
-    });
-})
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential)=>{
+        const user=userCredential.user;
+        const userData={
+            email: email,
+            firstName: firstName,
+            lastName:lastName
+        };
+        showMessage('Account Created Successfully', 'signUpMessage');
+        const docRef=doc(db, "users", user.uid);
+        setDoc(docRef,userData)
+        .then(()=>{
+            window.location.href='users/user_1.user_1.html';
+        })
+        .catch((error)=>{
+            console.error("error writing document", error);
 
-const analytics = getAnalytics(app);
+        });
+    })
+    .catch((error)=>{
+        const errorCode=error.code;
+        if(errorCode=='auth/email-already-in-use'){
+            showMessage('Email Address Already Exists !!!', 'signUpMessage');
+        }
+        else{
+            showMessage('unable to create User', 'signUpMessage');
+        }
+    })
+ });
 
-provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+ const signIn=document.getElementById('login_btn');
+ signIn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    const email=document.getElementById('email').value;
+    const password=document.getElementById('password').value;
+    const auth=getAuth();
 
-provider.setCustomParameters({
-    'login_hint': 'user@example.com'
-  });
-
-signInWithPopup(auth, provider)
-.then((result) => {
-// This gives you a Google Access Token. You can use it to access the Google API.
-const credential = GoogleAuthProvider.credentialFromResult(result);
-const token = credential.accessToken;
-// The signed-in user info.
-const user = result.user;
-// IdP data available using getAdditionalUserInfo(result)
-// ...
-}).catch((error) => {
-// Handle Errors here.
-const errorCode = error.code;
-const errorMessage = error.message;
-// The email of the user's account used.
-const email = error.customData.email;
-// The AuthCredential type that was used.
-const credential = GoogleAuthProvider.credentialFromError(error);
-// ...
-});
+    signInWithEmailAndPassword(auth, email,password)
+    .then((userCredential)=>{
+        showMessage('login is successful', 'signInMessage');
+        const user=userCredential.user;
+        localStorage.setItem('loggedInUserId', user.uid);
+        window.location.href='homepage.html';
+    })
+    .catch((error)=>{
+        const errorCode=error.code;
+        if(errorCode==='auth/invalid-credential'){
+            showMessage('Incorrect Email or Password', 'signInMessage');
+        }
+        else{
+            showMessage('Account does not Exist', 'signInMessage');
+        }
+    })
+ })
