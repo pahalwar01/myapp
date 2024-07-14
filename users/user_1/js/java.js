cf7c91dee71ff8cfa1f5c9a7d1373fc31a5ba76b
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-07-14 20:00:39 +0530

    Update java.js

--- users/user_1/js/java.js
+++ users/user_1/js/java.js
@@ -49,6 +49,7 @@ var logout = document.getElementById("logout");
 logout.onclick = function()
 {  
     localStorage.clear();
+    console.log.clear();
     sessionStorage.clear();
     var logout_text = document.getElementById("logout_text");
     logout_text.innerHTML = "Please wait...";
