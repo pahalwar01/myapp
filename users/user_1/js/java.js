8ea0c9423cbf694bfd00c24ff465319124566649
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-07-14 19:47:49 +0530

    Update java.js

--- users/user_1/js/java.js
+++ users/user_1/js/java.js
@@ -1,5 +1,46 @@
+var email = document.getElementById("login_uname");
+var pass = document.getElementById("login_pwd");
+
+var user = email, pass;
 
 
+window.onload = function()
+{
+    console.log(user);
+    if(console.log.getItem("user") == null)
+        {
+            window.location.replace("../../index.html");
+        }
+        else
+        {
+            var user_email = sessionStorage.getItem("user");
+            var json_data = localStorage.getItem(user_email);
+            var obj_data = JSON.parse(json_data);
+            var profile_name = document.getElementById("profile_name");
+            var rider_details = document.getElementById("rider_details");
+            var dharampal = document.getElementById("dharampal_details");
+            var karan = document.getElementById("karan_details");
+            var lalit = document.getElementById("lalit_details");
+
+
+            rider_details.innerHTML = atob(obj_data.username) + "'s Total Conveyance Details"
+            profile_name.innerHTML = "Welcome ";
+            
+            if(atob(obj_data.username)==("Dharampal"))
+            {
+                dharampal.style.display = "block";
+            }
+            if(atob(obj_data.username)==("Karan"))
+            {
+                karan.style.display = "block";
+            }
+            if(atob(obj_data.username)==("Lalit"))
+            {
+                lalit.style.display = "block";
+            }
+            
+        }
+}
 
 // Logout coding start
 
