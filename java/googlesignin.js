b206b361b02a585599d46a0b339d14dbe81740b7
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-07-14 15:26:22 +0530

    Update googlesignin.js

--- java/googlesignin.js
+++ java/googlesignin.js
@@ -1,18 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-import { getAuth, 
-         createUserWithEmailAndPassword, 
-         signInWithEmailAndPassword, 
-         GoogleAuthProvider,
-         signInWithPopup,
-         onAuthStateChanged,
-         signOut } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+ import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+ import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+ 
+ const firebaseConfig = {
     apiKey: "AIzaSyDXIbUIbKYmKVPUtfrzCPAxLb2t4r_Jlww",
     authDomain: "account-f83b4.firebaseapp.com",
     databaseURL: "https://account-f83b4-default-rtdb.firebaseio.com",
@@ -21,137 +12,82 @@ const firebaseConfig = {
     messagingSenderId: "374695878902",
     appId: "1:374695878902:web:72ea054fbd780f98650cd7",
     measurementId: "G-D0XY2VX61J"
-  };
-  
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-auth.languageCode = 'en'
-const provider = new GoogleAuthProvider();
+ };
 
-// logged in and logged out sections
-const loggedInView = document.getElementById('logged-in-view')
-const loggedOutView = document.getElementById('container')
-const userEmail = document.getElementById('user-email')
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
 
-// email and password for signin
-const emailSignInForm = document.getElementById('login_uname')
-const passwordSignInForm = document.getElementById('login_pwd')
+ function showMessage(message, divId){
+    var messageDiv=document.getElementById(divId);
+    messageDiv.style.display="block";
+    messageDiv.innerHTML=message;
+    messageDiv.style.opacity=1;
+    setTimeout(function(){
+        messageDiv.style.opacity=0;
+    },5000);
+ }
+ const signUp=document.getElementById('submitSignUp');
+ signUp.addEventListener('click', (event)=>{
+    event.preventDefault();
+    const email=document.getElementById('rEmail').value;
+    const password=document.getElementById('rPassword').value;
+    const firstName=document.getElementById('fName').value;
+    const lastName=document.getElementById('lName').value;
 
-// email and password for signup
-const emailSignUpForm = document.getElementById('email')
-const passwordSignUpForm = document.getElementById('password')
+    const auth=getAuth();
+    const db=getFirestore();
 
-// Buttons
-const signInGoogleBtn = document.getElementById('sign-in-with-google-btn')
-const signUpGoogleBtn = document.getElementById('sign-up-with-google-btn')
-const googleBtns = [signInGoogleBtn, signUpGoogleBtn]
-
-const createAccountBtn = document.getElementById('signup_btn')
-const loginBtn = document.getElementById('login_btn')
-const logoutBtn = document.getElementById('logout-button')
-
-// Detects state change
-
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
-      const uid = user.uid;
-      const email = user.email
-      loggedInView.style.display = 'block'
-      userEmail.innerText = email
-      loggedOutView.style.display = 'none'
-      // ...
-    } else {
-      // User is signed out
-      // ...
-      loggedInView.style.display = 'none'
-      loggedOutView.style.display = 'block'
-    }
-  });
-
-
-// Event Listeners for Buttons
-// Click on Create Account Button
-createAccountBtn.addEventListener('click', () => {
-    createUserWithEmailAndPassword(auth, emailSignUpForm.value, passwordSignUpForm.value)
-        .then((userCredential) => {
-            // Signed up 
-            const user = userCredential.user;
-            console.log(user)
-            // ...
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential)=>{
+        const user=userCredential.user;
+        const userData={
+            email: email,
+            firstName: firstName,
+            lastName:lastName
+        };
+        showMessage('Account Created Successfully', 'signUpMessage');
+        const docRef=doc(db, "users", user.uid);
+        setDoc(docRef,userData)
+        .then(()=>{
+            window.location.href='index.html';
         })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorMessage)
-            // ..
+        .catch((error)=>{
+            console.error("error writing document", error);
+
         });
-    console.log('Create Account Button Clicked')
-    console.log(`Email: ${emailSignUpForm.value}`)
-    console.log(`Password: ${passwordSignUpForm.value}`)
-})
+    })
+    .catch((error)=>{
+        const errorCode=error.code;
+        if(errorCode=='auth/email-already-in-use'){
+            showMessage('Email Address Already Exists !!!', 'signUpMessage');
+        }
+        else{
+            showMessage('unable to create User', 'signUpMessage');
+        }
+    })
+ });
 
-// Click on Login Button
-loginBtn.addEventListener('click', () => {
-    signInWithEmailAndPassword(auth, emailSignInForm.value, passwordSignInForm.value)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            console.log(user)
-            window.location.href='users/user_1/user_1.html';
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorMessage)
-        });
+ const signIn=document.getElementById('submitSignIn');
+ signIn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    const email=document.getElementById('email').value;
+    const password=document.getElementById('password').value;
+    const auth=getAuth();
 
-    console.log('Login Clicked')
-    console.log(`Email: ${emailSignInForm.value}`)
-    console.log(`Password: ${passwordSignInForm.value}`)
-})
-
-// Click on Google Signin
-googleBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        signInWithPopup(auth, provider)
-            .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const token = credential.accessToken;
-                // The signed-in user info.
-                const user = result.user;
-                console.log(user);
-                window.location.href='users/user_1/user_1.html';
-                // IdP data available using getAdditionalUserInfo(result)
-                // ...
-            }).catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorMessage);
-                // The email of the user's account used.
-                const email = error.customData.email;
-                // The AuthCredential type that was used.
-                const credential = GoogleAuthProvider.credentialFromError(error);
-                // ...
-            });
-        console.log('Google Signin Clicked');
-    });
-});
-
-/*
-// logout button
-logoutBtn.addEventListener('click', () => {
-    signOut(auth).then(() => {
-        // Sign-out successful.
-      }).catch((error) => {
-        // An error happened.
-      });
-      
-    console.log('Logout Clicked')
-})
-*/
+    signInWithEmailAndPassword(auth, email,password)
+    .then((userCredential)=>{
+        showMessage('login is successful', 'signInMessage');
+        const user=userCredential.user;
+        localStorage.setItem('loggedInUserId', user.uid);
+        window.location.href='homepage.html';
+    })
+    .catch((error)=>{
+        const errorCode=error.code;
+        if(errorCode==='auth/invalid-credential'){
+            showMessage('Incorrect Email or Password', 'signInMessage');
+        }
+        else{
+            showMessage('Account does not Exist', 'signInMessage');
+        }
+    })
+ })
