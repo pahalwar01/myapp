7b0c6fad7afc3455e9659439c8964df2984abf72
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-07-20 19:43:15 +0530

    Update googlesignin.js

--- java/googlesignin.js
+++
@@ -1,8 +0,0 @@
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  location.replace("users/user_1/user_1.html");
-  $("#profile_name").text(profile.getName());
-  $("#profile_pic").attr('scr', profile.getImageUrl());
-  $("#userpage").css("display","block");
-  $("#mainloginpage").css("display","none");
-}
