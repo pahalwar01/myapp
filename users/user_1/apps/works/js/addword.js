d9b09dcc5f5d02a024e5bf7316d9cc970b99102e
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-07-23 23:36:43 +0530

    Create addword.js

---
+++ users/user_1/apps/works/js/addword.js
@@ -0,0 +1,29 @@
+// Submit to google sheet 
+var submit_btn = document.getElementById("submit_btn");
+const scriptURL = 'https://script.google.com/macros/s/AKfycbydkPC0-s6ky0GdcqPwfEPpBAUFgXQ6pzq6_6tPhSL48xBr5ifD3ASgz6B58y-HoItO1A/exec'
+const form = document.forms['submit-to-google-sheet']
+const success = document.getElementById("success");
+var redirect_page = document.getElementById("jobs_bg");
+    form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        success.innerHTML="Data Successfully Submited";
+
+        setTimeout(function()
+        {
+            success.innerHTML="";
+        },2000)
+        form.reset();
+        window.location.replace("works.html");
+        redirect_page.style.display = "none";
+        function autoRefresh() {
+            window.location = window.location.href;
+            }
+                setInterval('autoRefresh()', 3000);
+        
+            })
+    .catch(error => console.error('Error!', error.message))
+    })
+
+// submit to google sheet end
