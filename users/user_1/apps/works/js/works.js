a1ac658872382ccf38b511fc65c08f202aa13e45
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2024-07-23 23:36:47 +0530

    Update works.js

--- users/user_1/apps/works/js/works.js
+++ users/user_1/apps/works/js/works.js
@@ -1,28 +1,3 @@
-window.onload = function(){
-            var user_email = sessionStorage.getItem("user");
-            var json_data = localStorage.getItem(user_email);
-            var obj_data = JSON.parse(json_data);
-            var profile_name = document.getElementById("view_jobs");
-            var dharampal = document.getElementById("dharampal");
-            var karan = document.getElementById("karan");
-            var lalit = document.getElementById("lalit");
-            
-            profile_name.innerHTML = "Welcome ";
-
-            if(atob(obj_data.username)==("Dharampal"))
-                {
-                    dharampal.style.display = "block";
-                }
-                if(atob(obj_data.username)==("Karan"))
-                {
-                    karan.style.display = "block";
-                }
-                if(atob(obj_data.username)==("Lalit"))
-                {
-                    lalit.style.display = "block";
-                }
-}
-
 var add_item = document.getElementById("addwork");
 
 add_item.onclick = function()
